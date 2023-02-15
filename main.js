102b7d4ad7c566aadb8061444afcf53e1b8f9cd7
Jose <josemanuel21302@gmail.com>
2023-02-14 21:31:14 -0800

    added snowball on click

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-const gameContainer = document.querySelector("#game");
+const gameContainer = document.querySelector("#game")
+
+gameContainer.addEventListener(`click`,() => {let snowball = document.createElement('img')
+snowball.src = 'assets/SnowBall.png'
+snowball.className = "snowBall" //gives snowball a smaller size lol
+// snowball.style.position = "absolute"; // set the snowball's position to absolute so we can change the 
+snowball.style.top = "250px"
+snowball.style.left = "20px" // these positions are so that the snowBall spawns on the snowman
+
+gameContainer.appendChild(snowball)
+})
 

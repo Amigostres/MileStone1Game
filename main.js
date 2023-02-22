c926de925ab527e3a488cc315d1f73b98eaa8999
Jose <josemanuel21302@gmail.com>
2023-02-21 20:26:48 -0800

    using the slope/linear equation instead

--- main.js
+++ main.js
@@ -14,19 +14,37 @@ gameContainer.addEventListener('click', (e) => {
     const x = e.clientX 
     const y = e.clientY 
 
+    //calculate the slope
+    const rise = y - 140
+    const run = x - 30
+
+    const slope = rise/run
+
+    const riseB = slope * 570  
+    console.log(riseB);
+    //x is always 600
+    const endX = 600
+    const endY = riseB - 140
+    console.log(`endY: ${endY}`)
+
+
+
+
     //using the Pythagorean theorem we the the distance from location (30,360) to mouse location
-    const distance = Math.sqrt((x) ** 2 + y ** 2)
+    const distance = Math.sqrt(endX ** 2 + endY ** 2)
+    console.log(`discance: ${distance}`);
     // console.log(`distance:${distance}`)
     const speed = distance / 10500; // adjust this value to change the animation duration
-    // console.log(`speed:${speed}`)
-    const increment = 600 // this is so that it 100% goes off
+    console.log(`speed:${speed}`)
+    //const increment = 600 // this is so that it 100% goes off
     //note this does not help lol
 
     let intervalId = setInterval(() => {
-      const snowballX = parseInt(snowball.style.left) // parseInt is to get rid of the 'px'
-      const snowballY = parseInt(snowball.style.top)
-      const deltaX = (x - snowballX) * speed //delta = (mouselocation - starting) * duration
-      const deltaY = (y - snowballY) * speed
+
+      const snowballX = parseFloat(snowball.style.left) // parseInt is to get rid of the 'px'
+      const snowballY = parseFloat(snowball.style.top)
+      const deltaX = (endX - snowballX) * speed //delta = (mouselocation - starting) * duration
+      const deltaY = (endY - snowballY) * speed
 
       
 
@@ -35,15 +53,10 @@ gameContainer.addEventListener('click', (e) => {
 
       snowball.style.left = `${snowballX + deltaX}px`
       snowball.style.top = `${snowballY + deltaY}px`
-      //while snowball's x is still in the screen It won't delete it
-      console.log(x);
-      console.log(y);
-      console.log(`snowball ${snowballX}, ${snowballY}`);
-      console.log(`delta:${deltaX}, ${deltaY}`);
-      if (snowballX >= 600 || snowballY >= 500) { // I don't want the snowball to go under so I delete iti f it does
+      if (snowballX >= 560 || snowballY >= 500) { // I don't want the snowball to go under so I delete iti f it does
         console.log(`out of the div`);
         clearInterval(intervalId) // stops interval 
         gameContainer.removeChild(snowball) // deletes the snowball
       }
-    }, 10)
+    }, 30)
   })

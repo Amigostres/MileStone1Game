5070b2e34e5f17af5f2fb4c6d30264ac397e3d35
Jose <josemanuel21302@gmail.com>
2023-02-22 13:19:13 -0800

    deleted the extra file

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const gameContainer = document.querySelector("#game")
-
-gameContainer.addEventListener('click', (e) => {
+gameContainer.addEventListener(`click`, (e) => {
     //creates the snowball
     const snowball = document.createElement('img')
     snowball.src = 'assets/SnowBall.png'
@@ -9,52 +8,46 @@ gameContainer.addEventListener('click', (e) => {
     snowball.style.top = '360px'
     snowball.style.left = '30px'
     gameContainer.appendChild(snowball)
+    
 
-    //getting mouse location
-    const x = e.clientX 
-    const y = e.clientY 
-    console.log(`y: ${y}`);
-    //calculate the slope
-    const rise = y - 140
+    //get the mouse location
+    const targetX = e.clientX
+    console.log(`x: ${targetX}`);
+    const targetY = e.clientY
+    console.log(`y: ${targetY}`);
 
-    const run = x - 30
-
-    const slope = rise/run
-
-    const riseB = slope * 570
-    console.log(`riseB:${riseB}`);
-    //x is always 600
+    //get the slope
+    //get rise
+    let rise = targetY - 360
+    let run = targetX - 30
+    let slope = rise/run
+    console.log(slope);
+    //the end of the page
     const endX = 600
-    const endY = riseB * slope
-    console.log(`endY: ${endY}`)
+    
+    //linear equation
+    // y = mx + b
+    let endY = (endX * slope) + 360
 
-
-
-
-    //using the Pythagorean theorem we the the distance from location (30,360) to mouse location
     const distance = Math.sqrt(endX ** 2 + endY ** 2)
-    // console.log(`distance:${distance}`)
     const speed = distance / 10500; // adjust this value to change the animation duration
-    console.log(`speed:${speed}`)
+
 
     let intervalId = setInterval(() => {
 
-      const snowballX = parseFloat(snowball.style.left) // parseInt is to get rid of the 'px'
-      const snowballY = parseFloat(snowball.style.top)
-      const deltaX = (endX - snowballX) * speed //delta = (mouselocation - starting) * duration
-      const deltaY = (endY - snowballY) * speed
+        const snowballX = parseFloat(snowball.style.left) // parseInt is to get rid of the 'px'
+        const snowballY = parseFloat(snowball.style.top)
+        const deltaX = (endX - snowballX) * speed //delta = (mouselocation - starting) * duration
+        const deltaY = (endY - snowballY) * speed
+  
+        snowball.style.left = `${snowballX + deltaX}px`
+        snowball.style.top = `${snowballY + deltaY}px`
 
-      
+        if (snowballX >= 560 || snowballY >= 500) { // I don't want the snowball to go under so I delete iti f it does
+            console.log(`out of the div`);
+            clearInterval(intervalId) // stops interval 
+            gameContainer.removeChild(snowball) // deletes the snowball
+          }
+      }, 30)
 
-
-
-
-      snowball.style.left = `${snowballX + deltaX}px`
-      snowball.style.top = `${snowballY + deltaY}px`
-      if (snowballX >= 560 || snowballY >= 500) { // I don't want the snowball to go under so I delete iti f it does
-        console.log(`out of the div`);
-        clearInterval(intervalId) // stops interval 
-        gameContainer.removeChild(snowball) // deletes the snowball
-      }
-    }, 30)
-  })
+})

cddfc6f25b08b9db86392d4771f66f5c3f43c02e
Jose <josemanuel21302@gmail.com>
2023-02-21 18:40:18 -0800

    added console logs

--- main.js
+++ main.js
@@ -11,25 +11,37 @@ gameContainer.addEventListener('click', (e) => {
     gameContainer.appendChild(snowball)
   
     //getting mouse location
-    const x = e.clientX
-    const y = e.clientY
+    const x = e.clientX 
+    const y = e.clientY 
 
     //using the Pythagorean theorem we the the distance from location (30,360) to mouse location
-    const distance = Math.sqrt(x ** 2 + y ** 2)
+    const distance = Math.sqrt((x) ** 2 + y ** 2)
     // console.log(`distance:${distance}`)
-    const speed = distance / 2500; // adjust this value to change the animation duration
+    const speed = distance / 10500; // adjust this value to change the animation duration
     // console.log(`speed:${speed}`)
     const increment = 600 // this is so that it 100% goes off
+    //note this does not help lol
 
     let intervalId = setInterval(() => {
       const snowballX = parseInt(snowball.style.left) // parseInt is to get rid of the 'px'
       const snowballY = parseInt(snowball.style.top)
       const deltaX = (x - snowballX) * speed //delta = (mouselocation - starting) * duration
       const deltaY = (y - snowballY) * speed
+
+      
+
+
+
+
       snowball.style.left = `${snowballX + deltaX}px`
       snowball.style.top = `${snowballY + deltaY}px`
       //while snowball's x is still in the screen It won't delete it
-      if (snowballX >= 600 || snowballY >= 450) { // I don't want the snowball to go under so I delete iti f it does
+      console.log(x);
+      console.log(y);
+      console.log(`snowball ${snowballX}, ${snowballY}`);
+      console.log(`delta:${deltaX}, ${deltaY}`);
+      if (snowballX >= 600 || snowballY >= 500) { // I don't want the snowball to go under so I delete iti f it does
+        console.log(`out of the div`);
         clearInterval(intervalId) // stops interval 
         gameContainer.removeChild(snowball) // deletes the snowball
       }
